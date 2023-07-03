d1ebb80c222c4dd9b93a68a56f004e054d570f2c
Zuribaby <chiamakanwobodo07@gmail.com>
2023-07-02 19:13:32 -0700

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""a Python script that takes in a URL and an email, 
-sends a POST request to the passed URL with the email as 
-a parameter, and displays the body 
-of the response (decoded in utf-8).
-Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
+"""A script that:
+- takes in a URL
+- sends a POST request to the passed URL
+- takes email as a parameter
+- displays the body of the response
 """
 import sys
 import urllib.parse
 import urllib.request
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
